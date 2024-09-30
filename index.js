ab460632b601f6a20788ad9e4a4200b01614272a
da vinci <84037196+daxvinci@users.noreply.github.com>
2024-09-30 20:01:07 +0100

    fixed env

--- index.js
+++ index.js
@@ -2,17 +2,32 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from 'pg';
 import axios from "axios";
+import dotenv from "dotenv";
 
+
+dotenv.config()
 const port = 3000;
 const app = express()
-const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "book",
-    password: "Elvinci@911",
-    port: 5433,
+
+
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
 })
-db.connect()
+
+pool.connect((err) => {
+    if (err) {
+        // If there's an error, log it and throw an error
+        console.error('Error connecting to the database: ', err.stack);
+        throw err; // Throws an error, causing the program to stop
+    } else {
+        console.log('Successfully connected to the database');
+        // Release the client back to the pool after query
+        // release();
+    }
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -22,17 +37,17 @@ let books = [];
 
 app.get('/', async (req,res)=>{
     try{
-        const result = await db.query("SELECT * FROM read_books")
+        const result = await pool.query("SELECT * FROM read_books")
         books = result.rows
         res.render('index.ejs',{
-            savedBooks:books
+            savepoolooks:books
         })
     }
     catch(error){
         console.log(error)
         books = []
         res.render('index.ejs',{
-            savedBooks:books
+            savepoolooks:books
         })
     }
     
@@ -80,15 +95,15 @@ app.post('/add', async (req,res)=>{
 app.get('/form', (req,res)=>{
     const details = req.query
     res.render('form.ejs',{
-        addbook:details,
+        adpoolook:details,
     })
 })
 
 app.post('/form', async(req,res)=>{
     const data = req.body
     try{
-        const result = await db.query("INSERT INTO read_books(book,author,publish_year,rating,isbn,fake_user) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",[data.book,data.author,data.year,data.rating,data.isbn,data.user])
-        // const user = await db.query("INSERT INTO users(user) VALUES($1) RETURNING *")
+        const result = await pool.query("INSERT INTO read_books(book,author,publish_year,rating,isbn,fake_user) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",[data.book,data.author,data.year,data.rating,data.isbn,data.user])
+        // const user = await pool.query("INSERT INTO users(user) VALUES($1) RETURNING *")
         console.log(result)
         res.redirect('/')
     }
@@ -100,7 +115,7 @@ app.post('/form', async(req,res)=>{
 
 app.get('/edit', async(req,res)=>{
     const data = parseInt(req.query.id)
-    const result = await db.query("SELECT * FROM read_books JOIN notes ON notes.book_id = read_books.id WHERE read_books.id = $1",[data])
+    const result = await pool.query("SELECT * FROM read_books JOIN notes ON notes.book_id = read_books.id WHERE read_books.id = $1",[data])
     if(result.rows[0] != undefined){
         res.render('edit.ejs',{
             note:result.rows[0]
@@ -117,18 +132,18 @@ app.post('/edit', async (req, res) => {
 
     try {
         // Check if the note exists
-        const result = await db.query("SELECT * FROM notes WHERE id = $1", [data.noteId]);
+        const result = await pool.query("SELECT * FROM notes WHERE id = $1", [data.noteId]);
 
         if (result.rows.length > 0) {
             // Note exists, update it
-            const valuesBack = await db.query("UPDATE notes SET note = $1 WHERE id = $2 RETURNING *", [data.updatedNote, data.noteId]);
-            const updateUser = await db.query("UPDATE read_books SET fake_user = $1 WHERE id = $2 RETURNING *", [data.user, result.rows[0].book_id]);
+            const valuesBack = await pool.query("UPDATE notes SET note = $1 WHERE id = $2 RETURNING *", [data.updatedNote, data.noteId]);
+            const updateUser = await pool.query("UPDATE read_books SET fake_user = $1 WHERE id = $2 RETURNING *", [data.user, result.rows[0].book_id]);
             console.log(valuesBack);
             console.log(updateUser);
         } else {
             // Note does not exist, insert new note
-            const valuesBack = await db.query("INSERT INTO notes(note, book_id) VALUES($1, $2) RETURNING *", [data.newNote, data.bookId]);
-            const newUser = await db.query("UPDATE read_books SET fake_user = $1 WHERE id = $2 RETURNING *", [data.user, data.bookId]);
+            const valuesBack = await pool.query("INSERT INTO notes(note, book_id) VALUES($1, $2) RETURNING *", [data.newNote, data.bookId]);
+            const newUser = await pool.query("UPDATE read_books SET fake_user = $1 WHERE id = $2 RETURNING *", [data.user, data.bookId]);
             console.log(valuesBack);
             console.log(newUser);
         }
@@ -143,19 +158,19 @@ app.post('/edit', async (req, res) => {
 
 // app.post('/edit', async(req,res)=>{
 //     const data = req.body
-//     const result = await db.query("SELECT * FROM notes")
+//     const result = await pool.query("SELECT * FROM notes")
 //     for(let i =0 ; i < result.rows.length ; i++){
 //         if(result.rows[i].id == data.noteId){
-//             const valuesBack = await db.query("UPDATE notes SET note = $1 WHERE book_id = $2 RETURNING *",[data.updatedNote,data.noteId])
-//             const updateUser = await db.query("UPDATE read_books SET fake_user = $1 WHERE id = $2 RETURNING *",[data.user,data.noteId])
+//             const valuesBack = await pool.query("UPDATE notes SET note = $1 WHERE book_id = $2 RETURNING *",[data.updatedNote,data.noteId])
+//             const updateUser = await pool.query("UPDATE read_books SET fake_user = $1 WHERE id = $2 RETURNING *",[data.user,data.noteId])
 //             console.log(valuesBack)
 //             console.log(updateUser)
 //             res.redirect('/')
 //             return
 //         }
 //     }
-//     const valuesBack = await db.query("INSERT INTO notes(note,book_id) VALUES($1,$2) RETURNING *",[data.newNote,data.bookId])
-//     const newUser = await db.query("UPDATE read_books SET fake_user = $1 WHERE id = $2 RETURNING *",[data.user,data.bookId])
+//     const valuesBack = await pool.query("INSERT INTO notes(note,book_id) VALUES($1,$2) RETURNING *",[data.newNote,data.bookId])
+//     const newUser = await pool.query("UPDATE read_books SET fake_user = $1 WHERE id = $2 RETURNING *",[data.user,data.bookId])
 //     console.log(valuesBack)
 //     console.log(newUser)
 //     res.redirect('/')
