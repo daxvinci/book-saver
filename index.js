a78c1bc89613549a19bd670bce59084586378923
da vinci <84037196+daxvinci@users.noreply.github.com>
2024-10-03 14:14:00 +0100

    update v1 - modal not showing

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false // necessary for connecting to Render apparently...sike gpt lied
-//   }
+  ssl: {
+    rejectUnauthorized: false // necessary for connecting to Render apparently...sike gpt lied
+  }
 })
 
 pool.connect((err) => {
